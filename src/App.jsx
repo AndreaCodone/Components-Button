4021a35b57fec9e59ec91cb01badc1ea5c70acd4
Andrea Coda <aerdnaadoc@hotmail.it>
2023-09-09 18:33:39 +0200

    add footer and other minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Card from "./components/Card/Card";
-import Button from "./components/Button/Button";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Card />
+      <Footer />
     </>
   );
 }
